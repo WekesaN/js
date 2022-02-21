93195db60320a419bd10f5449cbeb7c781003a22
WekesN <isaacnyongesa2048@gmail.com>
2022-02-21 23:59:54 +0300

    basic arithmetic operations

---
+++ funcbasics/main.js
@@ -0,0 +1,18 @@
+const add7 = (value) => {
+    return value + 7;
+}
+// console.log(add7(10));
+
+const multiply = (value1, value2) => {
+    return value1 * value2
+}
+// console.log(multiply(10, 20));
+
+const capitalize = (valueLetter) => {
+    return valueLetter.toUpperCase();
+}
+// console.log(capitalize("wekesa"));
+
+const lastLetter = (last) => {
+    return 
+}
