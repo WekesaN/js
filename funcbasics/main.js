c6090981879c36260bae4bbba2a5ca32991c87f0
WekesN <isaacnyongesa2048@gmail.com>
2022-02-22 00:51:48 +0300

    created the fizzybuzzy  game

--- funcbasics/main.js
+++ funcbasics/main.js
@@ -20,4 +20,24 @@ const lastLetter = (last) => {
     return last.charAt(0);
 }
 
-console.log(lastLetter("wekesa"))
+// console.log(lastLetter("wekesa"))
+
+//Fizzybuzz
+//output fizzy for numbers divisible by 3
+//Output buzz for numbers divisible by 5
+//output fizzybuzz for numbers divisible by 15 or both 3 and 5
+
+const input = parseInt(prompt("Enter your number you would like to fizzybuzz upto: "));
+
+for (let i = 1; i <= input; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzybuzzy");
+    } else if (i % 3 === 0) {
+        console.log("fizzy")
+    } else if (i % 5 === 0) {
+        console.log("buzzy")
+    } else {
+        console.log(i)
+    }
+
+}
