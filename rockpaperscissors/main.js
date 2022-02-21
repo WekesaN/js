b2f737f58cdea0ac46c59b0f9c5d7d17ff5d628c
WekesN <isaacnyongesa2048@gmail.com>
2022-02-22 01:31:27 +0300

    creat function computer play where the computer outputs either rock, paper or scissors

--- rockpaperscissors/main.js
+++ rockpaperscissors/main.js
@@ -1,7 +1,21 @@
 //Generate random number between between 1 and 3
 
-const randomNum = () => {
-    return Math.floor(Math.random() * 3) + 1;
+// const randomNum = () => {
+//     return Math.floor(Math.random() * 3) + 1;
+// }
+
+// console.log(randomNum());
+
+const computerPlay = () => {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    if (randomNumber === 1) {
+        console.log("Rock");
+    } else if (randomNumber === 2) {
+        console.log("Paper")
+    } else {
+        console.log("scissors")
+    }
+
 }
 
-console.log(randomNum());
+computerPlay();
